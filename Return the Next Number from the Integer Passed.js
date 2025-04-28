d801e8c53f2597534735089166983d82abeccc1f
ofek2700 <shmofek123@gmail.com>
2025-04-28 21:24:01 +0300

    Add files via upload

---
+++ Return the Next Number from the Integer Passed.js
@@ -0,0 +1,10 @@
+function addition(num) {
+    return num +1
+}
+console.log(addition(444))
+console.log(addition(1))
+console.log(addition(44))
+console.log(addition(8))
+console.log(addition(2))
+console.log(addition(4))
+console.log(addition(8))
