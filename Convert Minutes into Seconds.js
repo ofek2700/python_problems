42aa0a610c2e571680bd60921151f2bade6b1540
ofek2700 <shmofek123@gmail.com>
2025-04-28 21:24:25 +0300

    Add files via upload

---
+++ Convert Minutes into Seconds.js
@@ -0,0 +1,11 @@
+function convert(minutes) {
+    return minutes * 60
+}
+
+console.log(convert(11))
+console.log(convert(170))
+console.log(convert(14))
+console.log(convert(120))
+console.log(convert(1110))
+console.log(convert(144))
+console.log(convert(15))
